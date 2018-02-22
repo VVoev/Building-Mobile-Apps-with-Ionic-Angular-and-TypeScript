b6d1423ec12745c8ba1471a62ec38c538946a542
vladovoev <vladovoev@yahoo.co.uk>
2018-02-22 20:48:46 +0200

    added custom css

--- elite-schedule-app/src/pages/my-teams/my-teams.ts
+++ elite-schedule-app/src/pages/my-teams/my-teams.ts
@@ -1,6 +1,8 @@
+import { TeamHomePage } from './../team-home/team-home';
+import { EliteApi } from './../../app/shared/elite-api.service';
 import { TournamentsPage } from './../tournaments/tournaments';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the MyTeamsPage page.
@@ -16,7 +18,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyTeamsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  favorites = [
+    {
+      team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+      tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+      tournamentName: 'March Madness Tournament'
+    },
+    {
+      team: { id: 805, name: 'HC Elite', coach: 'Michelotti' },
+      tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+      tournamentName: 'Holiday Hoops Challenge'
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private eliteApi: EliteApi, private loadingController: LoadingController) {
   }
 
   ionViewDidLoad() {
@@ -25,6 +40,16 @@ export class MyTeamsPage {
 
   goToTournaments(){
     this.navCtrl.push(TournamentsPage);
+  }
+
+  favoriteTapped($event,team){
+    let loader = this.loadingController.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true
+    });
+    loader.present();
+    this.eliteApi.getTournamentData(team.tournamentId)
+    .subscribe(t=>this.navCtrl.push(TeamHomePage,team.team));
   }
 
 }
--- elite-schedule-app/src/pages/teams/teams.ts
+++ elite-schedule-app/src/pages/teams/teams.ts
@@ -1,8 +1,10 @@
+import { MyApp } from './../../app/app.component';
 import { EliteApi } from './../../app/shared/elite-api.service';
 import { TeamHomePage } from './../team-home/team-home';
 import { TeamDetailPage } from './../team-detail/team-detail';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as _ from 'lodash';
 
 /**
  * Generated class for the TeamsPage page.
@@ -19,6 +21,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class TeamsPage {
 
   teams = [];
+  private allTeams:any;
+  private allTeamDivisions:any;
   // [
   //   {id:1,name:"HC Elite"},
   //   {id:2,name:"The Bears"},
@@ -31,7 +35,14 @@ export class TeamsPage {
    let selectedTournament = this.navParams.data;
 
    this.eliteApi.getTournamentData(selectedTournament.id).subscribe(data=>{
-     this.teams = data.teams;
+     this.allTeams = data.teams;
+     this.allTeamDivisions =
+       _.chain(data.teams)
+         .groupBy('division')
+         .toPairs()
+         .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+         .value();
+     this.teams = this.allTeamDivisions;
    })
   }
 
